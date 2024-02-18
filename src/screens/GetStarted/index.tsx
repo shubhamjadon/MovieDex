b94188c836d70409cb47b4d4e67ee99a6e349081
Shubham Jadon <shubhamjadon99@gmail.com>
2024-02-18 19:37:54 +0530

    feat: added GetStarted screen

--- src/screens/GetStarted/index.tsx
+++ src/screens/GetStarted/index.tsx
@@ -1,10 +1,43 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, Dimensions, Pressable } from "react-native";
 import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import styles from "./styles";
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from "../../utils/constants";
+const GetStartedImage = require("../../../assets/images/getStarted.jpg");
 
 const GetStarted = () => {
+  const { navigate } = useNavigation();
+  const insets = useSafeAreaInsets();
+  const IMAGE_HEIGHT = SCREEN_HEIGHT - insets.bottom - insets.top - 2 * 64;
+  const IMAGE_WIDTH = SCREEN_WIDTH - insets.left - insets.right;
+
+  const goToHome = () => {
+    navigate("Home");
+  };
+
   return (
-    <View>
-      <Text>GetStarted</Text>
+    <View style={styles.container}>
+      <Image
+        style={[
+          styles.backgroundImage,
+          { height: IMAGE_HEIGHT, width: IMAGE_WIDTH },
+        ]}
+        source={GetStartedImage}
+      />
+      <View
+        style={[
+          styles.footer,
+          {
+            width: IMAGE_WIDTH,
+          },
+        ]}
+      >
+        <Text style={styles.title}>Uncover your next cinematic adventure.</Text>
+        <Pressable onPress={goToHome} style={styles.button}>
+          <Text style={styles.buttonText}>Get Started</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
--- src/screens/GetStarted/styles.ts
+++ src/screens/GetStarted/styles.ts
@@ -1,5 +1,40 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    position: "relative",
+  },
+  backgroundImage: {
+    marginTop: 32,
+  },
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    alignItems: "center",
+    paddingHorizontal: 24,
+    paddingBottom: 24,
+  },
+  title: {
+    color: "#f6f6f6",
+    fontSize: 24,
+    fontFamily: "Inter_500Medium",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#f6f6f6",
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    marginTop: 16,
+  },
+  buttonText: {
+    color: "#000",
+    fontFamily: "Inter_600SemiBold",
+    fontSize: 16,
+  },
+});
 
 export default styles;
